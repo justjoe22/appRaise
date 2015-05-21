29c3b2ff42a86640eec799f1dcb81acd19f8590a
justjoe22 <justjoe22@gmail.com>
2015-05-20 20:25:05 -0400

    New Changes

--- js/frmFunction.js
+++ js/frmFunction.js
@@ -6,6 +6,11 @@
 
 (function($) {
 
+	/* Click Function */
+	$(window).click(function onClickfnc(i) {
+		$('#message').html("<p>Selected: " + i + "</p>");
+	});
+	
 	/*---------------------------------------------------- */
 	/* Preloader
 	------------------------------------------------------ */
@@ -15,14 +20,11 @@
 
 		for (i = 0; i < arrayBooks.length; i++) {
 			var liHtml = "<p>" + arrayBooks[i] + "</p>";
-			liHtml = liHtml + "<button  type='button' id='button'" + arrayBooks[i] + ">Action</button>";
+			liHtml = liHtml + "<button  type='button' id='button'" + arrayBooks[i] + " onclick='onClickfnc(" + '"' + arrayBooks[i] + '"' + ")'>Action</button>";
 
 			$('#Book').append(liHtml);
 
-			//$('#button' + arrayBooks[i]).on('click', window.location.href = 'http://www.google.com');
-			
-		}
-
+		} //End of Loop
 
 	});
 
