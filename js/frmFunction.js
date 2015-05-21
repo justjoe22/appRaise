da5fa53fbabe7e2e3b264d30a3362da38acfe1ef
justjoe22 <justjoe22@gmail.com>
2015-05-21 13:23:23 -0400

    Function Creations

--- js/frmFunction.js
+++ js/frmFunction.js
@@ -11,15 +11,20 @@
 	------------------------------------------------------ */
 	$(window).load(function() {
 
-		/* Click Function */
+		/* Add the OnClick function to the host page. */
 		$('#mySCR').html('function onClickfnc(i) {$("#message").html("<p>Selected: " + i + "</p>");}');
 		
+		/* Add Books to Array */
 		var arrayBooks = ['Game of Thrones', 'Lord of the Rings', 'The Hobbit'];
 
+		/* Loop through each book, create a list item with an action button. */
 		for (i = 0; i < arrayBooks.length; i++) {
+			
+			/* Define the HTML for each book. */
 			var liHtml = "<p>" + arrayBooks[i] + "</p>";
 			liHtml = liHtml + "<button  type='button' id='button'" + arrayBooks[i] + " onclick='onClickfnc(" + '"' + arrayBooks[i] + '"' + ")'>Action</button>";
 
+			/* Post HTML code to the host page. */
 			$('#Book').append(liHtml);
 
 		} //End of Loop
