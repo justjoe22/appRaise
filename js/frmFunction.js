a478fa343114b5f558d541798b47ee998be1f0a8
Joe Prewitt <justjoe22@gmail.com>
2015-07-24 09:58:56 -0400

    Add and Remove
    Added a remove line from Array button. Added a textbox and add button.

--- js/frmFunction.js
+++ js/frmFunction.js
@@ -6,29 +6,87 @@
 
 (function($) {
 
+	var arrayBooks = [];
+
 	/*---------------------------------------------------- */
 	/* Preloader
 	------------------------------------------------------ */
 	$(window).load(function() {
 
 		/* Add the OnClick function to the host page. */
-		$('#mySCR').html('function onClickfnc(i) {$("#message").html("<p>Selected: " + i + "</p>");}');
+		//$('#mySCR').html('function onClickfnc(i) {$("#message").html("<p>Selected: " + i + "</p>");removeLine(i);}');
 		
 		/* Add Books to Array */
-		var arrayBooks = ['Game of Thrones', 'Lord of the Rings', 'The Hobbit', 'The Novel'];
+		arrayBooks.push('Game of Thrones');
+		arrayBooks.push('Lord of the Rings');
+		arrayBooks.push('The Hobbit');
+		arrayBooks.push('The Novel');
 
+		populateDiv();
+
+	});
+	
+	function removeLine(myVal) {
+		
+		arrayBooks = jQuery.grep(arrayBooks, function(value) {
+			return value != arrayBooks[myVal];
+		});
+		
+		populateDiv();
+	
+	}
+	
+	function addLine() {
+		
+		var myVal = document.getElementById('addTxt').value;
+		
+		arrayBooks.push(myVal);
+		
+		populateDiv();
+	
+	}
+	
+	function populateDiv() {
+	
+		/* Clear HTML */
+		$('#Book').html("");
+		
 		/* Loop through each book, create a list item with an action button. */
 		for (i = 0; i < arrayBooks.length; i++) {
 			
 			/* Define the HTML for each book. */
 			var liHtml = "<p>" + arrayBooks[i] + "</p>";
-			liHtml = liHtml + "<button  type='button' id='button'" + arrayBooks[i] + " onclick='onClickfnc(" + '"' + arrayBooks[i] + '"' + ")'>Action</button>";
+			liHtml = liHtml + "<button  type='button' id='button" + i + "' value='" + i + "'>Remove</button>";
 
 			/* Post HTML code to the host page. */
 			$('#Book').append(liHtml);
+			
+			var myButton = document.getElementById('button' + i);
+			if (myButton.addEventListener) {
+				myButton.addEventListener('click', function(e){removeLine(e.target.value);}, false);
+			} else {
+				myButton.attachEvent('onclick', function(e){removeLine(e.target.value);});
+			}
 
 		} //End of Loop
-
-	});
+		
+		var liCode = "<p>Add a book: <input type='text' id='addTxt' /><button type='button' id='addBtn'>Add</button></p>";
+		
+		$('#Book').append(liCode);
+		
+		var myButton2 = document.getElementById('addBtn');
+		if (myButton2.addEventListener) {
+			myButton2.addEventListener('click', function(e){addLine();}, false);
+		} else {
+			myButton2.attachEvent('onclick', function(e){addLine();});
+		}
+		
+		$("#addTxt").keypress(function(e) {
+			if(e.which == 13) {				
+				addLine();
+			}
+		});
+		
+	}
 
 })(jQuery);
